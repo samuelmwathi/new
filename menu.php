658c559bf0b2975938ce661df5e07e87845ecf09
samuelmwathi <seniormwathi@gmail.com>
2022-07-07 04:49:43 -0700

    thur

--- menu.php
+++ menu.php
@@ -55,24 +55,30 @@ public function apply_loan($textArray,$availableLoanToBorrow,$loanType,$phoneNum
         $response.="2 : (3)Months \n";
         echo $response;
      } else if($textArray[1]==1 && $level==2){
-
-        switch($loanType){
-            case 3: 
-                $response="END Dear Customer you are only allowed to topup your loan 
-                if you select payment period that match the current loan";
-                echo $response;
-            break;
-            default:{
-                if($this->amount_to_creadit<=0){
-                    $response="END you are not allowed to borrow, Your loan Limit is at KSH:".$this->amount_to_creadit;
-                   echo $response;
-                   } 
-                     else{
-                          $response="CON Enter amount between KSH: 100 and KSH:".$availableLoanToBorrow;
-                        echo $response;
-               }
-            }
+        $currentday=date('y-m-d');
+        if ($this->repayDate > $currentday ){
+            switch($loanType){
+                case 3: 
+                    $response="END Dear Customer you are only allowed to topup your loan 
+                    if you select payment period that match the current loan";
+                    echo $response;
+                break;
+                default:{
+                    if($this->amount_to_creadit<=0){
+                        $response="END you are not allowed to borrow, Your loan Limit is at KSH:".$this->amount_to_creadit;
+                       echo $response;
+                       } 
+                         else{
+                              $response="CON Enter amount between KSH: 100 and KSH:".$availableLoanToBorrow;
+                            echo $response;
+                   }
+                }
                   
+        }else{
+            echo "END hhhhdhdbbfvdh"
+        }
+
+            
         }
          //one month loan -----end------//   
       }else if($textArray[1]==2 && $level==2){
