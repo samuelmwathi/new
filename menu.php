463d48a9f12612ee6c6087f7cb86a9c3d1636e15
samuelmwathi <seniormwathi@gmail.com>
2022-07-06 23:27:25 -0700

    thur

--- menu.php
+++ menu.php
@@ -74,7 +74,7 @@ public function apply_loan($textArray,$availableLoanToBorrow,$loanType,$phoneNum
                      }
                 }else{
                     $current_date=date('y-m-d');
-                    if($this->repayDate>$current_date){
+                    if($this->repayDate >=$current_date){
                         if($this->amount_to_creadit<=0){
                             $response="END you are not allowed to borrow, Your loan Limit is at KSH:".$this->amount_to_creadit;
                             echo $response;
