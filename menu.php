4d11fa44d07c42d10b38c300c5e60d80756ad624
samuelmwathi <seniormwathi@gmail.com>
2022-07-07 01:40:25 -0700

    thur

--- menu.php
+++ menu.php
@@ -64,13 +64,13 @@ public function apply_loan($textArray,$availableLoanToBorrow,$loanType,$phoneNum
             default:{
                 if($repayDate==0){
                     if($this->amount_to_creadit<=0){
-                        $response="END you are not allowed to borrow, Your loan Limit is at KSH:".$this->amount_to_creadit
-                        ."   ".$this->repayDate;
+                        $response="END you are not allowed to borrow, Your loan Limit is at KSH:".$this->amount_to_creadit;
                         echo $response;
                         break;
                      } 
                      else{
-                        $response="CON Enter amount between ..........KSH: 100 and KSH:".$availableLoanToBorrow;
+                        $response="CON Enter amount between ..........KSH: 100 and KSH:".$availableLoanToBorrow
+                        ."   ".$this->repayDate;
                         echo $response;
                      }
                 }else{
