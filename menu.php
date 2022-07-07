679f438e1758d805cfba13ee6827b3a938f17ea8
samuelmwathi <seniormwathi@gmail.com>
2022-07-07 04:24:56 -0700

    th

--- menu.php
+++ menu.php
@@ -55,20 +55,7 @@ public function apply_loan($textArray,$availableLoanToBorrow,$loanType,$phoneNum
         $response.="2 : (3)Months \n";
         echo $response;
      } else if($textArray[1]==1 && $level==2){
-        switch($loanType){
-            case 3: 
-                 $response="END Dear Customer you are only allowed to topup your loan 
-                 if you select payment period that match the current loan";
-                 echo $response;
-                 break;
-            default:{
-                $response="END .................. Dear Customer you are only allowed to topup your loan 
-                 if you select payment period that match the current loan";
-                 echo $response;
-            }
-                
-                 
-        }
+        echo "END the devial is a liae";
             
       }else if($textArray[1]==2 && $level==2){
 
