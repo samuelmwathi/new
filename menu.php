d0527a510b8424273c8162708bbac8805971032d
samuelmwathi <seniormwathi@gmail.com>
2022-07-08 00:52:09 -0700

    fri

--- menu.php
+++ menu.php
@@ -212,7 +212,7 @@ public function apply_loan($textArray,$availableLoanToBorrow,$loanType,$phoneNum
 
     }else{
 //update the existing recode
-echo "END Dear customer please wait as your loan is being processed, Thank you!".$comparedate;
+echo "END Dear customer please wait as your loan is being processed, Thank you!";
 
         }
 
