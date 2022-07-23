f2eb111963c3312930367045b7de31e882e85a84
samuelmwathi <seniormwathi@gmail.com>
2022-07-23 17:00:21 +0300

    sat

--- menu.php
+++ menu.php
@@ -222,7 +222,7 @@ public function apply_loan($textArray,$availableLoanToBorrow,$loanType,$phoneNum
     }else{
             $sql =(" UPDATE `loan_status` as ls SET `loan_amount`= 400 WHERE ls.phone_number='+254723595220' 
             and ls.loan_status='-1';");
-             $execute=mysqli_query($conn,$sqlsmt);
+             $execute=mysqli_query($conn,$sql);
              if($execute){
                 echo "END successful";
         
