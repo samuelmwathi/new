12ae99bb877fa2cda85ee8469e4a584c85a801d1
samuelmwathi <seniormwathi@gmail.com>
2022-07-28 03:22:39 -0700

    remove pdo

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 //configuration ang connection to data base
-class Connect extends PDO{
+class Connect{
 
 public function __construct(){
-parent::__construct("mysql:host=remotemysql.com;dbname=62ufR36NQc",'62ufR36NQc','Q6jiKEZ2vP',
-array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8"));
-$this->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-$this->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    $conn=mysqli_connect(hostname:"remotemysql.com",
+    username: "62ufR36NQc",
+    password:"Q6jiKEZ2vP",
+    database:"62ufR36NQc");
 }
 
 }
